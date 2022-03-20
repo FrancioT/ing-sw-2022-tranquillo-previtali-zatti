718b7dad3164b9ae6c0a0b0892c0c64753aeab7f
ZattiAndrea <andrea1.zatti@mail.polimi.it>
2022-03-20 15:54:21 +0100

    Implemented Island

--- src/main/java/it/polimi/ingsw/CharacterCardJ.java
+++ src/main/java/it/polimi/ingsw/CharacterCardJ.java
@@ -5,6 +5,7 @@ import com.sun.tools.javac.util.List;
 public class CharacterCardJ extends CharacterCard{
     private List<Student> students;
 
-    public swapStudent() {}
-    }
+    public void swapStudent(){}
+
 }
+
---
+++ src/main/java/it/polimi/ingsw/Island.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw;
+
+public class Island extends Tile{
+    private Towers towers;
+    private int numTowers;
+    private boolean motherNatureFlag;
+    private boolean inhibitionFlag;
+
+    public void islandsLinker(){}
+    public colour dominance(){}
+    public void towersSwitcher(Towers t){}
+}
