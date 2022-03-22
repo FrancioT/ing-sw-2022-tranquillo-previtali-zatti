ac06b8030b5afeaf4a47af7c3749d7cba16a8220
andreazatti <andrea1.zatti@mail.polimi.it>
2022-03-22 21:52:02 +0100

    general implementation

--- src/main/java/it/polimi/ingsw/Deck.java
+++ src/main/java/it/polimi/ingsw/Deck.java
@@ -13,15 +13,15 @@ public class Deck {
         handList=new ArrayList<>();
 
         handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
-        handList.add(new StandardCard(1,1));
+        handList.add(new StandardCard(2,1));
+        handList.add(new StandardCard(3,2));
+        handList.add(new StandardCard(4,2));
+        handList.add(new StandardCard(5,3));
+        handList.add(new StandardCard(6,3));
+        handList.add(new StandardCard(7,4));
+        handList.add(new StandardCard(8,4));
+        handList.add(new StandardCard(9,5));
+        handList.add(new StandardCard(10,5));
     }
 
     public StandardCard cardDiscarder(int pos)
