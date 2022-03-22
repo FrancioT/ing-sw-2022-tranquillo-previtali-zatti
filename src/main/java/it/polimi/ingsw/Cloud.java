41b9366e95fafab2961d59a26b4b674384da1cd0
Denis-Previtali <denis.previtali@mail.polimi.it>
2022-03-22 15:04:12 +0100

    Cloud & Tile

--- src/main/java/it/polimi/ingsw/Cloud.java
+++ src/main/java/it/polimi/ingsw/Cloud.java
@@ -5,6 +5,21 @@ import java.util.List;
 public class Cloud extends Tile{
     private Bag bag;
 
-    public void cloudFiller(int n){}
-    public List<Student> cloudEmptier(it.polimi.ingsw.Dashboard.Dashboard d){}
+    public void cloudFiller(int n) {
+        for (int i = 0; i < n; i++) {
+            addStudent(bag.randomExtraction());
+        }
+    }
+
+    @Override
+    public void removeStudents() {
+        for (int i = 0; i < this.getStudents().size(); i++) {
+            this.getStudents().remove(0);
+        }
+    }
+
+    public void cloudEmptier(it.polimi.ingsw.Dashboard.Dashboard d){
+        d.entranceFiller(this.getStudents());
+        removeStudents();
+    }
 }
