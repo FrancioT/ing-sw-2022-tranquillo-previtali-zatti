9697f410155ebf8d92ecd8df602d7aa009ba61d3
ZattiAndrea <andrea1.zatti@mail.polimi.it>
2022-03-30 16:27:29 +0200

    Towers implementation

--- src/main/java/it/polimi/ingsw/Towers.java
+++ src/main/java/it/polimi/ingsw/Towers.java
@@ -1,13 +1,35 @@
 package it.polimi.ingsw;
 
-import java.util.List;
+import it.polimi.ingsw.Exceptions.FullTowersException;
 
 public class Towers {
     private int availableTowers;
     private final ColourT colour;
 
     public Towers(ColourT colour) { this.colour=colour; }
-    //public int availabilityChecker(){}
-    //public void availabilityModifier(int delta){}
+
+    public int availabilityChecker()
+    {
+        return availableTowers;
+    }
+
+    public void availabilityModifier(int delta) throws FullTowersException
+    {
+        if(availableTowers + delta < 0)
+        {
+            throw new FullTowersException();
+        }
+
+        if(availableTowers + delta > 8)
+        {
+            throw new FullTowersException();
+        }
+
+        if((availableTowers + delta >= 0) && (availableTowers + delta <= 8))
+        {
+            availableTowers = availableTowers + delta;
+        }
+    }
+
     public ColourT getColour() { return colour; };
 }
