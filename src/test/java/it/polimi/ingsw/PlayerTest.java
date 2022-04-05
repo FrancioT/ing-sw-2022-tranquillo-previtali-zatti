a0c11c529b07071c79ce6bc1ef0f9fff81b94c3f
Denis-Previtali <denis.previtali@mail.polimi.it>
2022-04-05 15:56:55 +0200

    completed player's tests

--- src/test/java/it/polimi/ingsw/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/PlayerTest.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.Exceptions.EmptyException;
-import it.polimi.ingsw.Exceptions.FullEntranceException;
-import it.polimi.ingsw.Exceptions.NoSuchStudentException;
+import it.polimi.ingsw.Exceptions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -12,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
 
     @Test
-    public void testGetStudentsNum(){
+    public void testGetStudentsNum() throws FullClassException {
         Player player = new Player("test", new Towers(ColourT.black, 1));
         player.setNickName("test");
         int num = player.getStudentNum(Colour.red);
@@ -32,14 +30,62 @@ class PlayerTest {
     @Test
     public void testEntranceFillerEmptier() throws EmptyException, NoSuchStudentException, FullEntranceException {
         Player player = new Player("test", new Towers(ColourT.black, 1));
+        Colour colour;
         List<Student> students= new ArrayList<>();
         students.add(new Student(Colour.blue));
         player.entranceFiller(students);
-        Colour colour = player.entranceEmptier(Colour.blue).getColour();
-        if(colour.equals(Colour.blue))
+        List<Colour> colours = new ArrayList<>();
+        colours = player.getStudents();
+        for(int i=0; i < colours.size(); i++){
+            colour = colours.get(i);
+            if (colour.equals(Colour.blue))
+                assertTrue(true);
+        }
+        Colour colour1 = player.entranceEmptier(Colour.blue).getColour();
+        if(colour1.equals(Colour.blue))
             assertTrue(true);
         else
             assertTrue(false);
+    }
+
+    @Test
+    public void testTeacherMovements() throws TeacherAlreadyInException, TooManyTeachersException, NoSuchTeacherException {
+        Player player = new Player("test", new Towers(ColourT.black, 1));
+        assertFalse(player.checkTeacherPresence(Colour.red));
+        player.addTeacher(new Teacher(Colour.red));
+        assertTrue(player.checkTeacherPresence(Colour.red));
+        try{
+            player.removeTeacher(Colour.blue);
+            assertTrue(false);
+        } catch (NoSuchTeacherException n){};
+
+        try{
+            player.removeTeacher(null);
+            assertTrue(false);
+        } catch (NullPointerException n){};
+
+        Teacher teachertest = player.removeTeacher(Colour.red);
+        if (teachertest.getColour().equals(Colour.red))
+            assertTrue(true);
+        else
+            assertTrue(false);
+    }
+
+    @Test
+    public void towersTest(){
+        Towers towers = new Towers(ColourT.black, 1);
+        Player player = new Player("test", towers);
+        assertTrue(player.getTowers().equals(towers));
+        Towers towers1 = new Towers(ColourT.white, 1);
+        assertFalse(player.getTowers().equals(towers1));
+    }
+
+    @Test
+    public void getLastCardMNValuetest(){
+        Player player = new Player("test", new Towers(ColourT.black, 1));
+        StandardCard sc = player.cardDiscarder(0);
+        int i = player.getLastCardMNValue();
+        assertEquals(i, 1);
     }
 
 }
